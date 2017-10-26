43dc3564f9d9bbd767f4c7ac7d6d32254934fc0a
huanglin <524180539@qq.com>
2017-10-26 15:10:54 +0800

    修复import

--- install.sql
+++ install.sql
@@ -1,3 +1,4 @@
+--任务表
 CREATE TABLE `t_task` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
@@ -19,7 +20,7 @@ CREATE TABLE `t_task` (
   KEY `idx_user_id` (`user_id`),
   KEY `idx_group_id` (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+--任务分组表
 CREATE TABLE `t_task_group` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
@@ -30,7 +31,7 @@ CREATE TABLE `t_task_group` (
   KEY `idx_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+--任务日志表
 CREATE TABLE `t_task_log` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `task_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID',
@@ -43,7 +44,7 @@ CREATE TABLE `t_task_log` (
   KEY `idx_task_id` (`task_id`,`create_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+--用户表
 CREATE TABLE `t_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(20) NOT NULL DEFAULT '' COMMENT '用户名',
@@ -57,5 +58,78 @@ CREATE TABLE `t_user` (
   UNIQUE KEY `idx_user_name` (`user_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--用户组表
+CREATE TABLE `t_user_group` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `grunp_name` varchar(20) NOT NULL DEFAULT '' COMMENT '用户组名',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for resource 资源表
+-- ----------------------------
+DROP TABLE IF EXISTS `t_resource`;
+CREATE TABLE `t_resource` (
+  `id` char(4) NOT NULL DEFAULT '',
+  `name` char(30) DEFAULT NULL,
+  `url` char(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for role 角色表
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role` (
+  `name` varchar(10) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for role_resource 角色资源表
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role_resource`;
+CREATE TABLE `t_role_resource` (
+  `role_id` int(11) NOT NULL DEFAULT '0',
+  `resource_id` char(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`role_id`,`resource_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for user_role用户角色表
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_role`;
+CREATE TABLE `t_user_role` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records
+-- ----------------------------
+--初始化用户
 INSERT INTO `t_user` (`id`, `user_name`, `email`, `password`, `salt`, `last_login`, `last_ip`, `status`)
 VALUES (1,'admin','admin@example.com','7fef6171469e80d32c0559f88b377245','',0,'',0);
+INSERT INTO `t_user` (`id`, `user_name`, `email`, `password`, `salt`, `last_login`, `last_ip`, `status`)
+VALUES (2,'guest','guest@example.com','3491931ed643ac6372b0af93e77adb9f','',0,'',0);
+
+INSERT INTO `t_resource` VALUES ('1001', '首页', 'index.jsp');
+INSERT INTO `t_resource` VALUES ('2001', '管理员页面', 'admin.jsp');
+INSERT INTO `t_resource` VALUES ('3001', '报表查询', 'report.jsp');
+INSERT INTO `t_resource` VALUES ('4001', '系统用户管理', 'userManager.jsp');
+INSERT INTO `t_role` VALUES ('ROLE_ADMIN', '1');
+INSERT INTO `t_role` VALUES ('ROLE_USER', '2');
+INSERT INTO `t_role_resource` VALUES ('1', '1001');
+INSERT INTO `t_role_resource` VALUES ('1', '2001');
+INSERT INTO `t_role_resource` VALUES ('1', '3001');
+INSERT INTO `t_role_resource` VALUES ('1', '4001');
+INSERT INTO `t_role_resource` VALUES ('2', '1001');
+INSERT INTO `t_role_resource` VALUES ('2', '3001');
+
+INSERT INTO `t_user_role` VALUES ('1', '1');
+INSERT INTO `t_user_role` VALUES ('2', '2');
+
